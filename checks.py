e7b4db224e333263ec038f69d39563c702eea1ef
QingyuChai <chai.qingyu00@gmail.com>
2017-05-31 19:16:54 +0200

    checks adds listeners now

--- checks.py
+++ checks.py
@@ -1,3 +1,7 @@
+import json
+
+from pydispatch import dispatcher
+
 __version__ = "1.0.0"
 __doc__ = """
 Checks for the Facebook bot
@@ -5,6 +9,21 @@ Checks for the Facebook bot
 
 def command(func):
     def check(func):
+        try:
+            with open('modules.json', 'r') as f:
+                modules = json.loads(f.read())
+                modules['modules'].append(func.__name__)
+        except Exception as e:
+            print("[-] Error loading modules file. Shutting down.")
+            exit()
+        with open('modules.json', 'w') as f:
+            f.write(json.dumps(modules,
+                           indent=4,
+                           sort_keys=True)
+                )
+        dispatcher.connect(func,
+                           signal=func.__name__,
+                           sender=dispatcher.Any)
         func.is_command = True
         return func
     return check(func)
