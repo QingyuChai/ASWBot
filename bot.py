6d7940722287656a7f5587ebe2c472c7414ba9a3
QingyuChai <chai.qingyu00@gmail.com>
2017-06-29 23:06:28 +0200

    fully modular, no more threads, ASYNCIO

--- bot.py
+++ bot.py
@@ -1,148 +1,160 @@
 import json
 import asyncio
-import threading
+import websockets
+import rethinkdb as r
+import importlib
+import inspect
 from types import *
 
 from pydispatch import dispatcher
+import checks
 import exceptions
 import contentTypes
-import rethinkdb as r
 
 # erase the modules file
+"""
 with open('modules.json', 'w') as f:
     f.write(json.dumps({
-        'modules' : []
+        'modules': []
     },
-                       indent=4,
-                       sort_keys=True)
-            )
+        indent=4,
+        sort_keys=True)
+    )
+"""
 
 __version__ = "2.0.0"
 
+
 class Bot:
+
     def __init__(self):
         # Initialize prints, so we know what is going on
         print("\n\n\n\n=====================================")
         self.id = "413050579062125"
         print("[+] Connecting to Database...")
+        self.initDatabase()
+        print("[+] Loading modules...")
+        self.initModules()
+        print("[+] Ready to accept commands!")
+        print("=====================================")
+
+    def initModules(self):
+        self.modules = {}
+        self.commands = {}
+        moduleList = json.loads(open('modules.json', 'r').read())["modules"]
+        for module in moduleList:
+            tempMod = importlib.import_module('modules.{}'.format(module))
+            tempMod.setup(self)
+
+    def initDatabase(self):
         self.c = r.connect()
         self.c.use("Facebook")
-        print("[+] Loading modules...")
-        import commands
-        self.modules = json.loads(open('modules.json','r').read())['modules']
-        print("[+] Initalizing listeners...")
-        self.initialize_listeners()
-        print("[+] Listening to input...")
-        self.thread_count = 0
-        self.listen()
 
-    def on_message(self,
-                   message):
+    def addModule(self,
+                  module):
+        self.modules[type(module).__name__] = module
+        myIter = inspect.getmembers(module, predicate=inspect.ismethod)
+        # print(myIter)
+        for myMethod in myIter:
+            methodFunc = myMethod[1]
+            methodName = methodFunc.__name__
+            if hasattr(methodFunc, "is_command"):
+                # do something with the method and class
+                tempCom = contentTypes.Command(func=methodFunc,
+                                               name=methodName,
+                                               module=module)
+                self.commands[tempCom.name] = tempCom
+
+        # print(self.modules)
+        # for command in
+
+    async def onMessage(self,
+                        message):
         """
         Event on_message:
             For every message sent, this function is called to process the
             message received.
         """
-        if message.author == self.id:
-            # We don't want the bot to answer it's own messages
-            return
         context = contentTypes.Context(message=message)
-        thread_id = "[Thread#{}]".format(self.thread_count)
-        print(thread_id+"[+] Message received. Content:\n"+message.content)
-        print(thread_id+"[+] Sending command signal...")
         try:
             # We want it to respond if it replies with a command that does not
             # exist or is not available
-            print(thread_id+"[+] Command \"{}\" accepted... Checking "
+            print("[+] Command \"{}\" accepted... Checking "
                   "for validity.".format(context.message.command))
-            if context.message.command not in self.modules:
+            if context.message.command not in self.commands:
                 raise exceptions.InvalidCommand
-            print(thread_id+"[+] Command valid, processing now...")
-            dispatcher.send(signal=context.message.command,
-                            ctx=context,
-                            args=context.message.arguments)
+            await self.runCommand(ctx=context)
+            print("[+] Command valid, processing now...")
         except exceptions.InvalidCommand:
             # Command doesn't exist
-            print(thread_id+"[-] Command '{}' doesn't exist."\
+            print("[-] Command '{}' doesn't exist."
                   .format(context.message.command))
             context.send("Sorry, invalid command.")
         except exceptions.InvalidArguments:
             # Invalid command arguments passed
-            print(thread_id+"[-] Command '{}' was passed with invalid arguments."\
+            print("[-] Command '{}' was passed with invalid arguments."
                   .format(context.message.command))
             context.send("Sorry, invalid arguments.")
-        except exceptions.Restart:
-            # Reload commands
-            context.send("Restarting...")
-            print("[=] Restarting...")
-            print("=====================================")
-            raise exceptions.Restart
         except Exception as e:
             # Unexpected error
-            print(thread_id+"[-] Unexpected Error occured. Error logs here:\n\n"+e)
+            print("[-] Unexpected Error occured. Error logs here:\n\n" + e)
             context.send("Sorry, an unexpected error has occured.")
 
-    def initialize_listeners(self):
-        """
-        Start the following event listeners:
-            on_message: called every time a message is sent/received
-        """
-        dispatcher.connect(self.on_message,
-                           signal='on_message',
-                           sender=dispatcher.Any)
+    async def runCommand(self,
+                         ctx):
+        command = ctx.message.command
 
-    def listen(self):
+        # ctx.args has to be rewritten with functools
+        await self.commands[command].run(ctx, ctx.message.arguments)
+
+    async def listen(self, socket, path):
         """
         This is the change dictionary that is returned
         {
-            'old_val': None,
-            'new_val': {
-                'id': '0b60b000-7cc2-4886-a8ea-533eeccdf270',
-                'info': '{}'
-            }
+            'sender' : {
+                'id' : foo
+            },
+            'timestamp' : foo,
+            recipient : {
+                'id' : foo
+            },
+            'message' : foo
+
         }
         """
-        feed = r.table("Messages").changes().run(self.c)
-        for change in feed:
-            self.thread_count += 1
-            initial_request = change['new_val']['info']
-            message = initial_request['message']
-            author = initial_request['sender']['id']
-            timestamp = initial_request['timestamp']
-            recipient = initial_request['recipient']['id']
-            #print(json.dumps(initial_request,
-            #                 indent=4,
-            #                 sort_keys=True))
-            message = contentTypes.Message(author=author,
-                                           message=message,
-                                           timestamp=timestamp,
-                                           recipient=recipient)
-            #dispatcher.send(signal='on_message',
-            #                message=message)
-            if message.author == self.id:
-                # We don't want the bot to answer it's own messages
-                continue
-            print("{}[+] Starting thread...".format("[Thread#{}]".format(
-                self.thread_count
-                )))
-            thread = threading.Thread(target=self.on_message,
-                                      args=(message,))
 
-            thread.start()
-
-def main():
-    while True:
+        response = await socket.recv()
         try:
-            Bot()
-        except exceptions.Restart:
-            pass
-        except Exception as e:
-            print("[-] Application crashed unexpectedly. Error logs here:\n\n")
-            print(e)
-            print("\n\n[+] Restarting...")
+            initial_request = json.loads(response.replace("'", "\""))
+        except:
+            return
+
+        author = initial_request['sender']['id']
+        timestamp = initial_request['timestamp']
+        recipient = initial_request['recipient']['id']
+        message = initial_request['message']
+
+        Message = contentTypes.Message(author=author,
+                                       message=message,
+                                       timestamp=timestamp,
+                                       recipient=recipient)
+
+        if Message.author == self.id:
+            # We don't want the bot to answer it's own messages
+            return
+
+        await self.onMessage(message=Message)
+
+
+def main(loop):
+    messageListener = websockets.serve(Bot().listen, 'localhost', 1111)
+    loop.run_until_complete(messageListener)
+    loop.run_forever()
+
 
 if __name__ == "__main__":
     try:
-        main()
+        loop = asyncio.get_event_loop()
+        main(loop)
     except KeyboardInterrupt:
         print("[-] Application exited due to manual keyboard input.")
